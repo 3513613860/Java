b3777c63635f8262f5e72564094e859291d248f0
3513613860 <3513613860@qq.com>
2021-01-05 22:46:04 +0800

    双层循环

--- 2021_1_5 PrintX/Demo09X.java
+++ 2021_1_5 PrintX/Demo09X.java
@@ -1,6 +1,8 @@
-import java.util.*;
+package demo01;
 
-public class Main{
+import java.util.Scanner;
+
+public class Demo09X {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
